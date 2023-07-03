ae030390c7b7593cac7f306516bf7366a2c90258
ludanfeng <ludanfeng@zj.tech>
2023-07-03 13:34:09 +0800

    add some exception tests

--- expr_test.go
+++ expr_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"github.com/google/cel-go/cel"
+	"github.com/stretchr/testify/assert"
 	"github.com/zhijingtech/expr/testdata"
 )
 
@@ -73,6 +74,70 @@ func TestExpr_Eval(t *testing.T) {
 			if !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("Expr.Eval() = %v, want %v", got, tt.want)
 			}
+		})
+	}
+}
+
+func TestExpr_NewExpr_Err(t *testing.T) {
+	tests := []struct {
+		name       string
+		expression string
+		options    []Option
+		wantErr    string
+	}{
+		{
+			name:       "variable not exist",
+			expression: "dummy == 1",
+			options:    []Option{ThisVariable()},
+			wantErr:    "ERROR: <input>:1:1: undeclared reference to 'dummy' (in container '')\n | dummy == 1\n | ^",
+		},
+		{
+			name:       "expr wrong",
+			expression: "dummy === 1",
+			options:    []Option{ThisVariable()},
+			wantErr:    "ERROR: <input>:1:9: Syntax error: token recognition error at: '= '\n | dummy === 1\n | ........^",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			e, err := NewExpr(tt.expression, tt.options...)
+			assert.Nil(t, e)
+			assert.Equal(t, tt.wantErr, err.Error())
+		})
+	}
+}
+
+func TestExpr_Eval_Err(t *testing.T) {
+	tests := []struct {
+		name       string
+		expression string
+		options    []Option
+		input      any
+		wantErr    string
+	}{
+		{
+			name:       "this not exist",
+			expression: "this.dummy == 1",
+			options:    []Option{ThisVariable()},
+			input:      map[string]any{},
+			wantErr:    "no such attribute(s): this",
+		},
+		{
+			name:       "variable not exist",
+			expression: "this.dummy == 1",
+			options:    []Option{ThisVariable()},
+			input:      map[string]any{"this": map[string]any{"value": 1}},
+			wantErr:    "no such key: dummy",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			e, err := NewExpr(tt.expression, tt.options...)
+			assert.NoError(t, err)
+
+			got, err := e.Eval(tt.input)
+			assert.Nil(t, got)
+			assert.Equal(t, tt.wantErr, err.Error())
 		})
 	}
 }
